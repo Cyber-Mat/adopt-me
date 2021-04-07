af8874bc5f379dd983e5cbd033adf6c3b62302f9
Olamide Olaniyan <olaniyanolamide@gmail.com>
2021-04-07 21:35:22 +0100

    introduction to react hooks

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet.jsx";
+import SearchParams from "./SearchParams";
 
 const App = () => (
   <div>
     <h1>Adopt me!</h1>
-    <Pet name="Cactus" animal="dog" breed="lhasa" />
-    <Pet name="Frodo" animal="cat" breed="burmese" />
-    <Pet name="Piske" animal="rabbit" breed="chinchilla" />
+    <SearchParams />
   </div>
 );
 
