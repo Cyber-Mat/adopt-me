6314351f5d92dc0fe303c3a41744a150b7518bbe
Olamide Olaniyan <olaniyanolamide@gmail.com>
2021-04-13 14:24:43 +0100

    added reach router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Link, Router } from '@reach/router';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => (
   <React.StrictMode>
     <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
+      <header>
+        <Link to='/'>Adopt me!</Link>
+      </header>
+      <Router>
+        <SearchParams path='/' />
+        <Details path='/details/:id' />
+      </Router>
     </div>
   </React.StrictMode>
 );
---
+++ src/Details.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Details = props => (
+  <pre>
+    <code>{JSON.stringify(props, null, 4)}</code>
+  </pre>
+);
+
+export default Details;
